1e44e75333a3e767557f59bde4c05d5f4d4a4b77
solis <lacus.soli@gmail.com>
2014-02-28 11:41:57 +0300

    some improvements

--- js/todo.js
+++ js/todo.js
@@ -1,113 +1,124 @@
 /**
-* todoapp Module
-*
-* Description
-*/
-angular.module('todoapp', ['ngRoute'])
+ * TodoApp Module
+ *
+ * Description
+ */
 
-.config(function($routeProvider) {
-	$routeProvider
-		.when('/', {
-			controller: 'ListController',
-			templateUrl: 'todo.html'
-		})
-		.when('/archive', {
-			controller: 'ArchiveController',
-			templateUrl: 'archive.html'
-		})
-		.otherwise({
-	    	redirectTo:'/'
-	    });
+var TodoApp = angular.module('todoapp', ['ngRoute'])
+
+TodoApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'ListController',
+            templateUrl: 'partials/todo.html'
+        })
+        .when('/archive', {
+            controller: 'ArchiveController',
+            templateUrl: 'partials/archive.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 })
 
-.controller('ListController', function($scope){
-	$scope.todos = angular.fromJson(localStorage.todos) || [];
-	$scope.levels = ['default', 'primary', 'info', 'warning', 'danger'];
+TodoApp.controller('ListController', ['$scope', 'TodoFactory', function ($scope, TodoFactory) {
+    $scope.todos =  TodoFactory.getTodos();
+    $scope.archive = TodoFactory.getArchive();
+    $scope.levels = ['default', 'primary', 'info', 'warning', 'danger'];
 
-	$scope.addTodo = function () {
-		$scope.todos.push({task: $scope.todoTask, done: false, level: $scope.levels[0]});
-		$scope.todoTask = '';
-	}
+    $scope.addTodo = function () {
+        $scope.todos.push({task: $scope.todoTask, done: false, level: $scope.levels[0]});
+        $scope.todoTask = '';
+    }
 
-	$scope.doneTodo = function (todo) {
-		todo.done = !todo.done;
-		$scope.saveTodos();
-	}
-
-	$scope.saveTodos = function () {
-		localStorage.todos = angular.toJson($scope.todos);
-	}
-
-	$scope.remaining = function() {
-	    var count = 0;
-	    angular.forEach($scope.todos, function(todo) {
-	      count += todo.done ? 0 : 1;
-	    });
-	    return count;
+    $scope.remaining = function () {
+        var count = 0;
+        angular.forEach($scope.todos, function (todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
     };
 
-    $scope.completed = function() {
-    	return $scope.todos.length - $scope.remaining();
+    $scope.completed = function () {
+        return $scope.todos.length - $scope.remaining();
     }
 
-    $scope.total = function() {
-    	return $scope.todos.length;
+    $scope.total = function () {
+        return $scope.todos.length;
     }
 
-    $scope.progress = function() {
-    	return Math.floor($scope.completed() / $scope.total() * 100);
+    $scope.progress = function () {
+        return Math.floor($scope.completed() / $scope.total() * 100);
     }
 
-    $scope.archive = function (todo) {
-    	$scope.todos.splice($scope.todos.indexOf(todo), 1);
-    	var todoArchive = angular.fromJson(localStorage.todoArchive);
-    	todoArchive.push(todo);
-    	localStorage.todoArchive = angular.toJson(todoArchive);
-    	$scope.saveTodos();
+    $scope.archiveOne = function (todo) {
+        $scope.todos.splice($scope.todos.indexOf(todo), 1);
+        $scope.archive.push(todo);
     }
 
-  	$scope.archiveAll = function() {
-  		var toArchive = $scope.todos.filter(function(todo) {
-  			return todo.done;
-  		});
+    $scope.archiveAll = function () {
+        var toArchive = $scope.todos.filter(function (todo) {
+            return todo.done;
+        });
 
-	    angular.forEach(toArchive, function(todo) {
-	    	$scope.todos.splice($scope.todos.indexOf(todo), 1);
-	    });
+        angular.forEach(toArchive, function (todo) {
+            $scope.todos.splice($scope.todos.indexOf(todo), 1);
+        });
 
-	    var todoArchive = angular.fromJson(localStorage.todoArchive) || [];;
-	    todoArchive = todoArchive.concat(toArchive);
-	    localStorage.todoArchive = angular.toJson(todoArchive);
-	    $scope.saveTodos();
-  	}
+        $scope.archive = archive.concat(toArchive);
+    }
 
-  	$scope.changeLevel = function(todo) {
-  		var index = $scope.levels.indexOf(todo.level);
-  		todo.level = $scope.levels[(index + 1) % $scope.levels.length];
-  		$scope.saveTodos();
-  	}
-})
+    $scope.changeLevel = function (todo) {
+        var index = $scope.levels.indexOf(todo.level);
+        todo.level = $scope.levels[(index + 1) % $scope.levels.length];
+    }
 
-.controller('ArchiveController', function($scope) {
-	$scope.todoArchive = angular.fromJson(localStorage.todoArchive);
+    $scope.updateTodos = function () {
+        localStorage.todos = angular.toJson($scope.todos);
+    }
 
-	$scope.saveArchive = function() {
-		localStorage.todoArchive = angular.toJson($scope.todoArchive);
-	}
+    $scope.updateArchive = function () {
+        localStorage.archive = angular.toJson($scope.archive);
+    }
 
-	$scope.removeTodo = function(todo) {
-		$scope.todoArchive.splice($scope.todoArchive.indexOf(todo), 1);
-		$scope.saveArchive();
-	}
+    $scope.$watch('todos', $scope.updateTodos, true);
+    $scope.$watch('archive', $scope.updateArchive, true);
+}]);
 
-	$scope.removeAll = function() {
-		$scope.todoArchive = [];
-		$scope.saveArchive();
-	}
-})
+TodoApp.controller('ArchiveController', ['$scope', 'TodoFactory', function ($scope, TodoFactory) {
+    $scope.archive = TodoFactory.getArchive();
 
-.controller('HeaderController', function($scope, $location) {
-    $scope.isActive = function(viewLocation) {
+    $scope.updateArchive = function () {
+        localStorage.archive = angular.toJson($scope.archive);
+    }
+
+    $scope.removeTodo = function (todo) {
+        $scope.archive.splice($scope.archive.indexOf(todo), 1);
+    }
+
+    $scope.removeAll = function () {
+        $scope.archive = [];
+    }
+
+    $scope.$watch('archive', $scope.updateArchive, true);
+}]);
+
+TodoApp.controller('HeaderController', function ($scope, $location) {
+    $scope.isActive = function (viewLocation) {
         return viewLocation === $location.path();
     };
-})
+});
+
+TodoApp.factory('TodoFactory', function () {
+    var Factory = {};
+
+    Factory.getTodos = function () {
+        return angular.fromJson(localStorage.todos) || [];
+    }
+
+    Factory.getArchive = function () {
+        return angular.fromJson(localStorage.archive) || [];
+    }
+
+    return Factory;
+});
